c966135ebd529262f693e50a6b09418e9bae0f0b
Yurii Rybachok <drummer2009@ukr.net>
2023-06-14 10:40:06 +0300

    todoList component

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,7 @@
   <AppFilters />
 
   <main class="app-main">
-    <ul class="todo-list">
-      <li class="todo-item todo-item--done">
-        <div class="todo-item__status">
-          <i class="bi bi-check2"></i>
-        </div>
-        <span class="todo-item__text">Learn the basics of Vue</span>
-        <button class="todo-item__remove-button">
-          <i class="bi bi-trash3"></i>
-        </button>
-      </li>
-      <li class="todo-item">
-        <div class="todo-item__status">
-          <i class="bi bi-check2"></i>
-        </div>
-        <span class="todo-item__text">Learn the basics of Typescript</span>
-        <button class="todo-item__remove-button">
-          <i class="bi bi-trash3"></i>
-        </button>
-      </li>
-      <li class="todo-item">
-        <div class="todo-item__status">
-          <i class="bi bi-check2"></i>
-        </div>
-        <span class="todo-item__text">Subscribe to the channel</span>
-        <button class="todo-item__remove-button">
-          <i class="bi bi-trash3"></i>
-        </button>
-      </li>
-    </ul>
+    <AppTodoList />
 
     <section class="add-todo">
       <button class="add-todo__show-form-button">
@@ -57,11 +29,13 @@
 import { defineComponent } from "vue";
 import AppHeader from "./components/AppHeader.vue";
 import AppFilters from "./components/AppFilters.vue";
+import AppTodoList from "./components/AppTodoList.vue";
 
 export default defineComponent({
   components: {
     AppHeader,
-    AppFilters
+    AppFilters,
+    AppTodoList
   }
 })
 </script>
