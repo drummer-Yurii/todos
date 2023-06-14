02a5d06d542a2fd4c33dbbcde5c781a27744a433
Yurii Rybachok <drummer2009@ukr.net>
2023-06-14 10:42:28 +0300

    addTodo component

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,7 @@
   <main class="app-main">
     <AppTodoList />
 
-    <section class="add-todo">
-      <button class="add-todo__show-form-button">
-        <i class="bi bi-plus-lg"></i>
-      </button>
-      <form class="add-todo__form">
-        <button class="close-button" type="button">
-          <i class="bi bi-x"></i>
-        </button>
-        <div class="text-input text-input--focus">
-          <input class="input" />
-        </div>
-        <button class="button button--filled">Add task</button>
-      </form>
-    </section>
+    <AppAddTodo />
   </main>
 
   <footer class="app-footer">2 more to do, 1 done</footer>
@@ -30,12 +17,14 @@ import { defineComponent } from "vue";
 import AppHeader from "./components/AppHeader.vue";
 import AppFilters from "./components/AppFilters.vue";
 import AppTodoList from "./components/AppTodoList.vue";
+import AppAddTodo from "./components/AppAddTodo.vue";
 
 export default defineComponent({
   components: {
     AppHeader,
     AppFilters,
-    AppTodoList
+    AppTodoList,
+    AppAddTodo
   }
 })
 </script>
