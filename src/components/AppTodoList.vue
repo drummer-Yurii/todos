33575c070df0a8ad3c7f8fe906b318ca55167c51
Yurii Rybachok <drummer2009@ukr.net>
2023-06-14 14:02:49 +0300

    create click for choice todo with emit to parent

--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,6 +1,11 @@
 <template>
     <ul class="todo-list">
-        <AppTodoItem v-for="todo in todos" :key="todo.id" :todo="todo" />
+        <AppTodoItem 
+            v-for="todo in todos" 
+            :key="todo.id" 
+            :todo="todo"
+            @toggle-todo="toggleTodo" 
+        />
     </ul>
 </template>
 
@@ -25,6 +30,15 @@ export default defineComponent({
                 { id: 1, text: 'Learn the basics of Typescript', completed: false },
                 { id: 2, text: 'Subscribe to the channel', completed: false },
             ]
+        }
+    },
+
+    methods: {
+        toggleTodo(id: number) {
+            const targetTodo = this.todos.find((todo: Todo) => todo.id === id)
+            if (targetTodo) {
+                targetTodo.completed = !targetTodo.completed
+            }
         }
     },
 })
